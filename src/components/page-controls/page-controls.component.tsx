557b030f36dcaf017b0d0e24b91a14735a3b35e1
Ilia Kurganskii <ilya.kurganskii@gmail.com>
2022-06-18 18:04:24 +0200

    feat(components): add forwarded refs (#42)

--- src/components/page-controls/page-controls.component.tsx
+++ src/components/page-controls/page-controls.component.tsx
@@ -9,24 +9,30 @@ export interface PageControlsProps extends ViewProps {
   selectedIndex: number;
 }
 
-function PageControlsComponent(props: PageControlsProps) {
-  const theme = useTheme();
-  const { count, selectedIndex, style, ...viewProps } = props;
-  const containerStyle = getContainerStyle({ theme });
-  return (
-    <View style={StyleSheet.compose(containerStyle, style)} {...viewProps}>
-      {new Array(count).fill(0).map((_, index) => (
-        <View
-          key={index}
-          style={getPageItemStyle({
-            theme,
-            isSelected: index === selectedIndex,
-            isFirst: index === 0,
-          })}
-        />
-      ))}
-    </View>
-  );
-}
+const PageControlsComponent = React.forwardRef<View, PageControlsProps>(
+  (props, ref) => {
+    const theme = useTheme();
+    const { count, selectedIndex, style, ...viewProps } = props;
+    const containerStyle = getContainerStyle({ theme });
+    return (
+      <View
+        ref={ref}
+        style={StyleSheet.compose(containerStyle, style)}
+        {...viewProps}
+      >
+        {new Array(count).fill(0).map((_, index) => (
+          <View
+            key={index}
+            style={getPageItemStyle({
+              theme,
+              isSelected: index === selectedIndex,
+              isFirst: index === 0,
+            })}
+          />
+        ))}
+      </View>
+    );
+  }
+);
 
 export const PageControls = React.memo(PageControlsComponent);
